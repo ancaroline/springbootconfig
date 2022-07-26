6ad29fc8aad0fe195d8b6da03a48fe96db4514a2
Anne Caroline <carolaragao94@gmail.com>
2022-07-26 15:37:07 -0300

    Mapear variável de ambiente

--- springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/config/AppController.java
+++ springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/config/AppController.java
@@ -9,8 +9,21 @@ public class AppController {
     @Value("${app.message}")//vamos injetar todos os valores com esta anotação cujo o seja valor app.message
     private String appMessage;
 
+//Mapear variável de ambiente
+    @Value("${ENV_DB_URL: NENHUMA}")
+    private String dbEnvironmentVariable;
+
     @GetMapping("/")
     public String getAppMessage() {
         return appMessage;
     }
+
+    //Para fazer exibição da variável
+
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable() {
+        return "A seguinte variável de ambiente foi passada: " + dbEnvironmentVariable;
+    }
+
+
 }
