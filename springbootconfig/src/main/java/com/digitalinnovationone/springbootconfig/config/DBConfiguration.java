ad6a9e8da6eefba2abbf6bb13ca6de099465d447
Anne Caroline <carolaragao94@gmail.com>
2022-07-26 10:56:45 -0300

    declarando os métodos

--- springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/config/DBConfiguration.java
+++ springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/config/DBConfiguration.java
@@ -1,7 +1,9 @@
 package com.digitalinnovationone.springbootconfig.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ConfigurationProperties("spring.datasource")
@@ -11,4 +13,14 @@ public class DBConfiguration {
     private String url;
     private String username;
     private String password;
+    //Declarando os métodos que vão fazer o mapeamento das propriedades dentro
+    //do nosso sistema
+    @Profile("dev") //para pegar todas as propriedades de config com a propriedade profile
+    @Bean //desejamos instanciar esse método como bean, pois queremos mostrar esse conteúdo sendo mapeado logo na subida do nosso sistema
+    public String testDatabaseConnection() {
+        System.out.println("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to H2_TEST - Test instance";
+    }
 }
